e0e67cc7449064d7af2e3a94f2bb1af7255dbf99
Chlunidia Putri Kieswendha <chlunidia.putri.ft21@mail.umy.ac.id>
2022-12-23 16:47:58 +0700

    Change rear to my last name.

--- Exe5_064_Kelompok3/Program.cs
+++ Exe5_064_Kelompok3/Program.cs
@@ -15,36 +15,36 @@ namespace QueueLinkedList
             this.next = null;
         }
     }
-    // A class to represent a queue. The queue, chlunidia stores the chlunidia node of LL and rear stores the last node of LL
+    // A class to represent a queue. The queue, chlunidia stores the chlunidia node of LL and kieswendha stores the last node of LL
     class Queue
     {
-        public QNode chlunidia, rear;
+        public QNode chlunidia, kieswendha;
         public Queue() 
         { 
-            this.chlunidia = this.rear = null; 
+            this.chlunidia = this.kieswendha = null; 
         }
         // Method to add an key to the queue.
         public void enqueue(string key)
         {
             // Create a new LL node
             QNode temp = new QNode(key);
-            // If queue is empty, then new node is chlunidia and rear both
-            if (this.rear == null)
+            // If queue is empty, then new node is chlunidia and kieswendha both
+            if (this.kieswendha == null)
             {
                 Console.Write(key + " is inserted to queue.");
                 Console.WriteLine();
-                this.chlunidia = this.rear = temp;
+                this.chlunidia = this.kieswendha = temp;
                 return;
             }
             else
             {
                 Console.WriteLine("Please enter an element!");
             }
-            // Add the new node at the end of queue and change rear
+            // Add the new node at the end of queue and change kieswendha
             Console.Write(key + " is inserted to queue.");
             Console.WriteLine();
-            this.rear.next = temp;
-            this.rear = temp;
+            this.kieswendha.next = temp;
+            this.kieswendha = temp;
         }
         // Method to remove an key from queue.
         public void dequeue()
@@ -58,9 +58,9 @@ namespace QueueLinkedList
             // Store previous chlunidia and move chlunidia one node ahead
             Console.WriteLine("\nThe element deleted from the queue is.");
             this.chlunidia = this.chlunidia.next;
-            // If chlunidia becomes NULL, then change rear also as NULL
+            // If chlunidia becomes NULL, then change kieswendha also as NULL
             if (this.chlunidia == null)
-                this.rear = null;
+                this.kieswendha = null;
         }
         public void display()
         {
