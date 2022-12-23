02843e4748ac51a847e48f9bc300e5a81aea5f3f
Chlunidia Putri Kieswendha <chlunidia.putri.ft21@mail.umy.ac.id>
2022-12-23 16:29:29 +0700

    Create variable key and next

--- Exe5_064_Kelompok3/Program.cs
+++ Exe5_064_Kelompok3/Program.cs
@@ -5,6 +5,8 @@ namespace QueueLinkedList
     // A linked list (LL) node to store a queue entry
     class QNode
     {
-
+        // Create variable key and next
+        public string key;
+        public QNode next;
     }
 }
