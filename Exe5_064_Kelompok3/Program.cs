b801be0064a59bb8e39d4f649eb09e098646842c
Chlunidia Putri Kieswendha <chlunidia.putri.ft21@mail.umy.ac.id>
2022-12-23 16:39:46 +0700

    Create condition if the queue is not empty the new node placed at the end of queue and change rear.

--- Exe5_064_Kelompok3/Program.cs
+++ Exe5_064_Kelompok3/Program.cs
@@ -15,31 +15,36 @@ namespace QueueLinkedList
             this.next = null;
         }
     }
-    // A class to represent a queue. The queue, chlunidia stores the chlunidia node of LL and kieswendha stores the last node of LL
+    // A class to represent a queue. The queue, chlunidia stores the chlunidia node of LL and rear stores the last node of LL
     class Queue
     {
-        public QNode chlunidia, kieswendha;
+        public QNode chlunidia, rear;
         public Queue() 
         { 
-            this.chlunidia = this.kieswendha = null; 
+            this.chlunidia = this.rear = null; 
         }
         // Method to add an key to the queue.
         public void enqueue(string key)
         {
             // Create a new LL node
             QNode temp = new QNode(key);
-            // If queue is empty, then new node is chlunidia and kieswendha both
-            if (this.kieswendha == null)
+            // If queue is empty, then new node is chlunidia and rear both
+            if (this.rear == null)
             {
                 Console.Write(key + " is inserted to queue.");
                 Console.WriteLine();
-                this.chlunidia = this.kieswendha = temp;
+                this.chlunidia = this.rear = temp;
                 return;
             }
             else
             {
                 Console.WriteLine("Please enter an element!");
-            }            
+            }
+            // Add the new node at the end of queue and change rear
+            Console.Write(key + " is inserted to queue.");
+            Console.WriteLine();
+            this.rear.next = temp;
+            this.rear = temp;
         }
     }
 }
