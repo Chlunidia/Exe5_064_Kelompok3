62b6108c772b1a5d2fdc308a7e0b94aaccf95859
Chlunidia Putri Kieswendha <chlunidia.putri.ft21@mail.umy.ac.id>
2022-12-23 16:32:45 +0700

    Create constructor to create a new linked list node

--- Exe5_064_Kelompok3/Program.cs
+++ Exe5_064_Kelompok3/Program.cs
@@ -8,5 +8,11 @@ namespace QueueLinkedList
         // Create variable key and next
         public string key;
         public QNode next;
-    }
+        // constructor to create a new linked list node
+        public QNode(string key)
+        {
+            this.key = key;
+            this.next = null;
+        }
+    }    
 }
